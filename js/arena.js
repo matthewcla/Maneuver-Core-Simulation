80837718c37208aeb508d3d618691c629704f24a
matthewcla <133473967+matthewcla@users.noreply.github.com>
2025-07-15 12:54:14 -0400

    Increase drag delay

--- js/arena.js
+++ js/arena.js
@@ -1687,7 +1687,7 @@ class Simulator {
                     this.containerDragStart = { x: e.clientX, y: e.clientY };
                     this.containerDragActive = true;
                 }
-            }, 400);
+            }, 800);
         }
     }
 
