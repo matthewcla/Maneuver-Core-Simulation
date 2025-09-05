e09c2ff951493c96748f0b3843dac9700f261418
mikeclark21 <mikeclark21@gmail.com>
2025-09-05 11:47:48 -0400

    Handle fetch failures with offline page

--- sw.js
+++ sw.js
@@ -34,6 +34,9 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(resp => {
+      if (resp) return resp;
+      return fetch(event.request).catch(() => caches.match('/offline.html'));
+    })
   );
 });
